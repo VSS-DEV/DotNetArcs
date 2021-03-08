222acca4ab4c2cf36c73748da884d2bf689d4cfa
VSS-DEV <serega1349@yandex.ru>
2021-03-08 21:19:20 +0300

    Добавьте файлы проекта.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security;
+
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: AssemblyTrademark("")]
+[assembly: CompilationRelaxations(8)]
+[assembly: AssemblyCompany("Dino Chiesa, VSS-DEV")]
+[assembly: AssemblyProduct("DotNetZip Library")]
+[assembly: AssemblyCopyright("Copyright Â© Dino Chiesa 2006 - 2010, VSS-DEV 2021")]
+[assembly: AssemblyFileVersion("1.9.1.5")]
+[assembly: AllowPartiallyTrustedCallers]
+[assembly: AssemblyTitle("Ionic's Managed Zlib")]
+[assembly: AssemblyConfiguration("Retail")]
+[assembly: AssemblyDescription("library for Deflate and ZLIB compression.")]
+[assembly: CLSCompliant(true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
+[assembly: AssemblyVersion("1.9.1.5")]
---
+++ README.md
@@ -0,0 +1,4 @@
+# Ionic.Zlib v1.9.1.5
+Powered by https://www.nuget.org/profiles/gurplh
+<br>
+Fork by VSS-DEV for commpile to .NET Standard 2.1 and .NET Framework 2.0
