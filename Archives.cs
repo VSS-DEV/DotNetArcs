41cdbbc3fb033cdfd14d585f1550537970736fb6
VSS-DEV <serega1349@yandex.ru>
2021-10-19 17:39:54 +0300

    Chenge and testing new futures

---
+++ Archives.cs
@@ -0,0 +1,130 @@
+﻿using Ionic.Zlib;
+using System.ComponentModel;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DotNetArcs
+{
+    public enum ArchivFormat
+    {
+        Zlib
+    }
+
+    public enum ArchivFunction
+    {
+        [Description("Производит сжатие данных")]
+        Compress,
+        [Description("Производит растяжение данных")]
+        Decompress
+    }
+
+    public sealed class Archives
+    {
+        public Archives(ArchivFormat archivFormat, ArchivFunction archivFunction, byte[] SomeDataByte, out byte[] SomeArcDataByte)
+        {
+            switch (archivFormat)
+            {
+                case ArchivFormat.Zlib:
+                    new ZlibArc(archivFunction, SomeDataByte, out byte[] DataAck);
+                    SomeArcDataByte = DataAck;
+                    break;
+                default:
+                    SomeArcDataByte = null;
+                    break;
+            }
+        }
+        public Archives() { }
+
+        public Task<byte[]> ArchivesAsync(ArchivFormat archivFormat, ArchivFunction archivFunction, byte[] SomeDataByte)
+        {
+            
+
+            switch (archivFormat)
+            {
+                case ArchivFormat.Zlib:
+                    var AsyncZlib = new ZlibArc();
+                    return AsyncZlib.ZlibArcAsync(archivFunction, SomeDataByte);
+                default:
+                    return null;
+            }
+        }
+    }
+
+    sealed class ZlibArc
+    {
+        internal ZlibArc(ArchivFunction archivFunction, byte[] SomeDataByte, out byte[] SomeArcDataByte)
+        {
+            switch (archivFunction)
+            {
+                case ArchivFunction.Compress:
+                    SomeArcDataByte = Compressd(SomeDataByte);
+                    break;
+                case ArchivFunction.Decompress:
+                    SomeArcDataByte = Decompressd(SomeDataByte);
+                    break;
+                default:
+                    SomeArcDataByte = null;
+                    break;
+            }
+        }
+        internal ZlibArc() { }
+
+        internal async Task<byte[]> ZlibArcAsync(ArchivFunction archivFunction, byte[] SomeDataByte)
+        {
+            switch (archivFunction)
+            {
+                case ArchivFunction.Compress:
+                    
+                    return await CompressdAsync(SomeDataByte);
+                case ArchivFunction.Decompress:
+                    return await DecompressdAsync(SomeDataByte);
+                default:
+                    return null;
+            }
+        }
+
+        #region Асинхронные функции
+        private async Task<byte[]> CompressdAsync(byte[] SomeDataByte)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                var ZlibStream = new ZlibStream(new MemoryStream(SomeDataByte), CompressionMode.Compress, CompressionLevel.Level9);
+                await Task.Delay(200);
+                await ZlibStream.CopyToAsync(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+
+        private async Task<byte[]> DecompressdAsync(byte[] SomeDataByte)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                var ZlibStram = new ZlibStream(new MemoryStream(SomeDataByte), CompressionMode.Decompress);
+                await Task.Delay(200);
+                await ZlibStram.CopyToAsync(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+        #endregion
+
+        #region Синхронные функции
+        private byte[] Compressd(byte[] SomeDataByte)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                new ZlibStream(new MemoryStream(SomeDataByte), CompressionMode.Compress, CompressionLevel.Level9).CopyTo(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+
+        private byte[] Decompressd(byte[] SomeDataByte)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                new ZlibStream(new MemoryStream(SomeDataByte), CompressionMode.Decompress).CopyTo(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+        #endregion
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# DotNetArcs v2.0.0.12
+# DotNetArcs v2.1.0.32
 <br>
-Forked Ionic.Zlib by VSS-DEV for commpile to .NET Standard 2.1 and .NET Framework 2.0
+Forked Ionic.Zlib by VSS-DEV for commpile to .NET Standard 2.0|2.1 and .NET Framework 4.6
+<br>
+Тестовая версия!
